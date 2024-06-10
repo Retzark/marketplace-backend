dc934fbc490d5e69876c0ae2cec344f1a3bb012d
Gerard Baluyot <gacbaluyot@gmail.com>
2024-06-10 16:49:10 +0800

    added logs in trx

--- bot/sidechain/index.js
+++ bot/sidechain/index.js
@@ -6,7 +6,7 @@ const blockProcessor = async (data, scClient, state) => {
 
   for (let i = 0; i < data.length; i += 1) {
     const trx = data[i];
-    console.log("trx", trx.sender)
+    console.log("trx", trx)
     if (trx.contract === 'tokens' && trx.action === 'transfer' && trx.payload.to === config.ACCOUNT) {
       console.log("trx", trx);
       await tokensTransfer(trx, scClient);
